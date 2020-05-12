31dc8c4127d624e5325d5e6da753553a0ab458d0
Istvan Sipos <ccsistvan@cctk.org>
2020-05-13 01:01:36 +0200

    Additional tests with pregenerated keysets

--- src/test/java/com/stephensipos/ibiza/ThrowAwayTests.java
+++ src/test/java/com/stephensipos/ibiza/ThrowAwayTests.java
@@ -1,5 +1,6 @@
 package com.stephensipos.ibiza;
 
+import com.stephensipos.ibiza.algorithms.RSA;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
@@ -11,4 +12,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ThrowAwayTests {
 
+    @Test
+    public void genKeys() {
+        Integer[] sizes = new Integer[] {16,32,64,128,256,521,1024,2048};
+
+        for (var size: sizes) {
+            var keys = RSA.generateKeys(size);
+            System.out.println(String.format("new KeySet(\"%d\", \"%d\", \"%d\"), /* %d */", keys[0], keys[1], keys[2], size));
+        }
+
+    }
 }
